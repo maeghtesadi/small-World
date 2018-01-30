8759d941d89e6feb1026b349ffd77139090c77a8
Unknown <ma_eghtesadi@yahoo.com>
2018-01-30 11:56:58 -0500

    initial commit to dev branch

---
+++ smallWorld/smallWorld/Race.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+// we will use race as an abstraction and use inheritence to make specific races
+class race
+{
+protected:
+private:
+public:
+	//name of the race
+	string name;
+
+};
