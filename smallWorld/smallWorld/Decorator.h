e7c64b9473798871277413f8f2e7c9376b7688a9
Unknown <ma_eghtesadi@yahoo.com>
2018-04-03 22:45:37 -0400

    3: Auto stash before merge of "dev3" and "origin/dev3"

--- smallWorld/smallWorld/Decorator.h
+++ smallWorld/smallWorld/Decorator.h
@@ -1,1 +1,39 @@
 #pragma once
+#include <string>
+
+using namespace std; 
+
+class Display
+{
+public:
+	virtual string message() = 0;
+
+private:
+
+};
+
+class AddOnDecorator : public Display {
+public:
+	virtual string message() = 0;
+};
+
+// Display the percentage of regions owned/lost for each player,
+// update the view when any player conquers / loses a region.
+class DominationObserverDecorator: public AddOnDecorator {
+public:
+	virtual string message() = 0;
+};
+
+//Display the cards owned by every player, update the view when
+//any player�s hand is changing.
+class HandsObserverDecorator : public AddOnDecorator {
+public:
+	virtual string message() = 0;
+};
+
+//Display what player victory coins he/she scores, update the view
+//when any more coins earned by a player.
+class CoinsObserverDecorator : public AddOnDecorator {
+public:
+	virtual string message() = 0;
+};
